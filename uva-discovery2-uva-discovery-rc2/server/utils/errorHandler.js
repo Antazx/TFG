31c0ec1acb8be730912acaad2a409e5cee08f6a4
GuillermoAnta <guillermo.anta@alumnos.uva.es>
2020-09-20 19:58:01 +0200

    Last changes

--- uva-discovery2-uva-discovery-rc2/server/utils/errorHandler.js
+++
@@ -1,8 +0,0 @@
-import log from '../utils/logger';
-
-const logger = log.logger;
-
-export default function errorHandler(err, req, res, next) {
-  logger.error(`${JSON.stringify(err)}`, { label: 'ErrorHandler' });
-  res.status(err.status || 500).send({ error: err.message });
-}
